c5b5e8175aac6a70f9b750b53502c0422a5603c8
Vrushal <kulkarni.v@husky.neu.edu>
2016-06-03 17:35:56 -0400

    Fixed link issue

--- public/assignment/views/page/page-list.controller.client.js
+++ public/assignment/views/page/page-list.controller.client.js
@@ -1,23 +1,22 @@
-( function () {
+
+(function () {
     angular
         .module("WebAppMaker")
-        .controller("PageListController", PageListController);
+        .controller("PageListController",PageListController);
 
-    function PageListController ($routeParams, PageService, $location) {
+    function PageListController($routeParams,PageService, $location) {
         var vm = this;
-
         vm.userId = $routeParams.userId;
-        vm.websiteId = $routeParams.websiteId;
-
+        vm.webSiteId = $routeParams.websiteId;
         vm.linkToPageNew = linkToPageNew;
 
-        function  init() {
-            vm.pages = angular.copy(PageService.findPageByWebsiteId(vm.websiteId));
-        } init();
-        
-        function linkToPageNew() {
-            $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page/new");
+        function init() {
+            vm.pages = angular.copy(PageService.findPageByWebsiteId(vm.webSiteId));
         }
-    }
+        init();
 
+        function linkToPageNew() {
+             $location.url("/user/" + vm.userId + "/website/" + vm.webSiteId + "/page/new");
+         }
+    }
 })();
