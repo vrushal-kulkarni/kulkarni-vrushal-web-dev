287b4c496722b3d0d9481abbba12686f6a67f6cf
Vrushal <kulkarni.v@husky.neu.edu>
2016-06-08 14:48:33 -0400

    homepage

---
+++ public/project/config.js
@@ -0,0 +1,23 @@
+(function () {
+    angular
+        .module("GreAppMaker")
+        .config(Config);
+
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "views/user/login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
+            })
+            .when("/login", {
+                templateUrl: "views/user/login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
+            })
+            .otherwise({
+                redirectTo: "/login"
+            });
+
+    }
+})();
