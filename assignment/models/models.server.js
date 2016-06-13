7c85b61ac49793624e76690b58da1b80f3751b3b
Vrushal <kulkarni.v@husky.neu.edu>
2016-06-12 21:23:27 -0400

    openshift

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -1,9 +1,18 @@
 
 module.exports = function(){
 
-    var mongoose = require("mongoose");
+    var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    mongoose.connect('mongodb://localhost/cs5610summer1');
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+
+    var mongoose = require("mongoose");
+    mongoose.connect(connectionString);
 
     var models = {
         userModel: require("./user/user.model.server.js")(),
